49240c31f4684005df1a0c58f0845c62d8056ac4
Phillip Oertel <me@phillipoertel.com>
2011-07-01 13:16:03 +0200

    pass in map at marker construction time

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -6,10 +6,10 @@ function addReferencePoints(map, geocoder, adressesArray) {
       var markerOptions = {
         'position': results[0].geometry.location,
         'title': address,
-        'icon': icon('green')
+        'icon': icon('green'),
+        'map': map
       };
       var marker = new google.maps.Marker(markerOptions);
-      marker.setMap(map);
     });    
   });
 }
@@ -18,59 +18,5 @@ function icon(color) {
   return new google.maps.MarkerImage(
     "http://www.google.com/intl/en_us/mapfiles/ms/micons/" + color + "-dot.png",
     new google.maps.Size(32, 32)
-  );
-}
-
-// thanks john resig. taken from: http://ejohn.org/blog/one-week-of-google-maps-2/
-function resizeMap(map, points) {
-  var minLong = 999;
-  var minLat = 999;
-  var maxLong = -999;
-  var maxLat = -999;
-  
-  // Get the current map width/height
-  var size = map.getSpanLatLng();
-  var mapWidth = size.width;
-  var mapHeight = size.height;
-  var baseWidth = mapWidth;
-  var baseHeight = mapHeight;
-  
-  // Figure out the elemental unit (depends on the size of the map)
-  // You will need to re-run resizeMap() if the size of the map changes.
-  if ( map.getZoomLevel() > 0 ) {
-    baseWidth /= Math.pow( 2, map.getZoomLevel() );
-    baseHeight /= Math.pow( 2, map.getZoomLevel() );
-  }
-  
-  // Find the max/min points
-  for ( var i = 0; i < points.length; i++ ) {
-    if ( points[i].x < minLong ) minLong = points[i].x;
-    if ( points[i].x > maxLong ) maxLong = points[i].x;
-    if ( points[i].y < minLat ) minLat = points[i].y;
-    if ( points[i].y > maxLat ) maxLat = points[i].y;
-  }
-  
-  // Find the optimal Width Zoom
-  var wZoom = 0;
-  var w = Math.abs( maxLong - minLong );
-  for ( var i = 1; i < 16; i++ ) {
-    if ( baseWidth > w ) break;
-    baseWidth *= 2;
-    wZoom = i;
-  }
-  
-  // Find the optimal Height Zoom
-  var hZoom = 0;
-  var h = Math.abs( maxLat - minLat );
-  for ( var i = 1; i < 16; i++ ) {
-    if ( baseHeight > h ) break;
-    baseHeight *= 2;
-    hZoom = i;
-  }
-  
-  // Reposition
-  map.centerAndZoom(
-    new GPoint( ( minLong + maxLong ) / 2, ( minLat + maxLat ) / 2 ),
-    ( wZoom > hZoom ? wZoom : hZoom )
   );
 }
