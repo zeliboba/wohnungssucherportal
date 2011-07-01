06169f498b83862380c0adcca612a755d83a819b
Phillip Oertel <me@phillipoertel.com>
2011-07-01 12:23:33 +0200

    Upgraded Google maps to V3

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -1,38 +1,76 @@
-function initMap(width, height) {
-	if (!GBrowserIsCompatible()) { return; }
-  // init
-  var map = new GMap2(
-    document.getElementById("map_canvas"),
-    { 'size': new GSize(width, height)}
-  );
-  // set UI
-  map.addControl(new GMapTypeControl());
-  map.addControl(new GScaleControl());
-  map.addControl(new GSmallZoomControl());
-  map.removeMapType(G_HYBRID_MAP);
-	return map;
-}
-
 // FIXME don't pass in everything and the kitchen sink
-function addReferencePoints(map, geocoder, pointsArray) {
+function addReferencePoints(map, geocoder, adressesArray) {
   // add some city reference points
-  pointsArray.each(function(point) {
-    geocoder.getLatLng(point, function(latLng) {
+  adressesArray.each(function(address) {
+    geocoder.geocode({'address': address}, function(results, status) {
       var markerOptions = {
-        'title': point,
+        'position': results[0].geometry.location,
+        'title': address,
         'icon': icon('green')
       };
-      var marker = new GMarker(latLng, markerOptions);
-      map.addOverlay(marker);
+      var marker = new google.maps.Marker(markerOptions);
+      marker.setMap(map);
     });    
   });
 }
 
 function icon(color) {
-  var icon = new GIcon(G_DEFAULT_ICON);
-  icon.image = "http://www.google.com/intl/en_us/mapfiles/ms/micons/" + color + "-dot.png";
-  icon.iconSize = new GSize(34, 34);
-  icon.shadow = false;
-  return icon;
+  return new google.maps.MarkerImage(
+    "http://www.google.com/intl/en_us/mapfiles/ms/micons/" + color + "-dot.png",
+    new google.maps.Size(32, 32)
+  );
 }
 
+// thanks john resig. taken from: http://ejohn.org/blog/one-week-of-google-maps-2/
+function resizeMap(map, points) {
+  var minLong = 999;
+  var minLat = 999;
+  var maxLong = -999;
+  var maxLat = -999;
+  
+  // Get the current map width/height
+  var size = map.getSpanLatLng();
+  var mapWidth = size.width;
+  var mapHeight = size.height;
+  var baseWidth = mapWidth;
+  var baseHeight = mapHeight;
+  
+  // Figure out the elemental unit (depends on the size of the map)
+  // You will need to re-run resizeMap() if the size of the map changes.
+  if ( map.getZoomLevel() > 0 ) {
+    baseWidth /= Math.pow( 2, map.getZoomLevel() );
+    baseHeight /= Math.pow( 2, map.getZoomLevel() );
+  }
+  
+  // Find the max/min points
+  for ( var i = 0; i < points.length; i++ ) {
+    if ( points[i].x < minLong ) minLong = points[i].x;
+    if ( points[i].x > maxLong ) maxLong = points[i].x;
+    if ( points[i].y < minLat ) minLat = points[i].y;
+    if ( points[i].y > maxLat ) maxLat = points[i].y;
+  }
+  
+  // Find the optimal Width Zoom
+  var wZoom = 0;
+  var w = Math.abs( maxLong - minLong );
+  for ( var i = 1; i < 16; i++ ) {
+    if ( baseWidth > w ) break;
+    baseWidth *= 2;
+    wZoom = i;
+  }
+  
+  // Find the optimal Height Zoom
+  var hZoom = 0;
+  var h = Math.abs( maxLat - minLat );
+  for ( var i = 1; i < 16; i++ ) {
+    if ( baseHeight > h ) break;
+    baseHeight *= 2;
+    hZoom = i;
+  }
+  
+  // Reposition
+  map.centerAndZoom(
+    new GPoint( ( minLong + maxLong ) / 2, ( minLat + maxLat ) / 2 ),
+    ( wZoom > hZoom ? wZoom : hZoom )
+  );
+}
