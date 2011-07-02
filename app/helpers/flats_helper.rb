898f4653873636f4908286e5e15db19be6c40cec
Phillip Oertel <me@phillipoertel.com>
2011-07-02 18:11:46 +0200

    Highlight markers on the map when hovering over the street name.

--- app/helpers/flats_helper.rb
+++ app/helpers/flats_helper.rb
@@ -3,4 +3,10 @@ module FlatsHelper
     return "unlimited" unless flat.available_months
     "#{flat.available_months} month#{flat.available_months == 1 ? '' : 's'}"
   end
+  
+  def flats_data_for_client(flats)
+    flats.map do |flat|
+      {:address => flat.full_address, :dbId => flat.id}
+    end
+  end
 end
