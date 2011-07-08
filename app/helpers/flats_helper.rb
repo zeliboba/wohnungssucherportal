2d96173ffe83e6e4ddf739ffeaff3bc8d6cc4d8d
Phillip Oertel <me@phillipoertel.com>
2011-07-08 21:04:00 +0200

    Workaround the encoding prob

--- app/helpers/flats_helper.rb
+++ app/helpers/flats_helper.rb
@@ -4,9 +4,12 @@ module FlatsHelper
     "#{flat.available_months} month#{flat.available_months == 1 ? '' : 's'}"
   end
   
-  def flats_data_for_client(flats)
-    flats.map do |flat|
-      {:address => flat.full_address, :dbId => flat.id}
-    end
+  def flats_as_json(flats)
+    # to_json double encodes stuff and then google maps can't locate some addresses
+    # FIXME revert to #to_json once db encoding is fixed
+    inner = flats.map do |flat|
+      %({"address": "#{flat.full_address}", "dbId": "#{flat.id}"})
+    end.join(",\n")
+    "[ #{inner} ]"
   end
 end
