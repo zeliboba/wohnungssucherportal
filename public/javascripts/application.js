8f421afb48886695af2af403f68bab81ecd253b1
Phillip Oertel <me@phillipoertel.com>
2011-07-09 17:26:29 +0200

    Added date/time picker for editing a flat.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(function() {
+  var options = { 'dateFormat': 'dd.mm.yy' };
+  $("#flat_visit_at").datetimepicker(options);
+  $("#flat_available_on").datepicker(options);
+  $("#flat_available_until").datepicker(options);
+});
