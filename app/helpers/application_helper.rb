7d444fe71f83c73f71fc4051f069798748aa6ee0
Phillip Oertel <me@phillipoertel.com>
2011-07-09 20:06:22 +0200

    Added another reference point.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def betterplace_office_address
-    "Schlesische Straße 26, Berlin"
-  end
-  
-  def zwischenmiete_neukoelln_adress
-    "Weisestraße 53, Berlin"
-  end
   
   def amazon_muenchen
     "Marcel-Breuer-Straße 12, München"
+  end
+  
+  def xenia_muenchen
+    "Gabelsbergerstrasse 48, München"
   end
 end
