906a44cacaccc34fa04139fa97c283e880ec14c4
Phillip Oertel <me@phillipoertel.com>
2011-07-15 22:24:40 +0200

    Fixed spec

--- spec/requests/flats/sort_flat_spec.rb
+++ spec/requests/flats/sort_flat_spec.rb
@@ -6,13 +6,13 @@ feature "Sorting flats" do
     Factory.create(:flat, :street => 'Cheap', :price => 300)
     Factory.create(:flat, :street => 'Expensive', :price => 500, :created_at => Time.now - 1.minute)
     visit(root_path)
-    within("#flats_interesting") do
+    within("#flats_new") do
       # by default, the most recently created flat comes first (created_at DESC)
       find(:li, 1).should have_content("Cheap")
       find(:li, 2).should have_content("Expensive")
     end
     click_on('price')
-    within("#flats_interesting") do
+    within("#flats_new") do
       # price is configured to sort descending (price DESC)
       find(:li, 1).should have_content("Expensive")
       find(:li, 2).should have_content("Cheap")
