bcec1dcbd3b89b401edcf305f35aaf709bf766a5
Phillip Oertel <me@phillipoertel.com>
2011-07-18 16:02:42 +0200

    Using test data which has an until date.

--- spec/models/wg_gesucht_page_scraper_spec.rb
+++ spec/models/wg_gesucht_page_scraper_spec.rb
@@ -1,24 +1,46 @@
 require 'spec_helper'
 
-describe PageScraper::WGGesucht do
+describe "it parses shared flats" do
 
-  it "parses basic attributes from given HTML page" do
-    url = "http://www.wg-gesucht.de/my_url"
-    flat = PageScraper::WGGesucht.from_hpricot(hpricot_doc_for('neue-bahnhofstrasse'), url)
-    assert_equal url, flat[:url]
-    assert_equal Flat::STATES.first, flat[:state]
-    
-    assert_equal 18, flat[:square_meters]
-    assert_equal Date.parse("06.07.2011"), flat[:available_on]
-    assert_equal 303, flat[:price]    
-    assert_equal "Friedrichshain", flat[:neighbourhood]
-    assert_equal "Neue Bahnhofstr. 33", flat[:street]
+  before(:all) do
+    @url = "http://www.wg-gesucht.de/my_url"
+    @flat = PageScraper::WGGesucht.from_hpricot(hpricot_doc_for('occamstrasse'), @url)
   end
+  
+  it "stores basic attributes in the flat model" do
+    assert_equal @url, @flat[:url]
+    assert_equal Flat::STATES.first, @flat[:state]
+  end
+  
+  it "parses square_meters" do
+    assert_equal 12, @flat[:square_meters]
+  end
+  
+  it "parses dates" do
+    assert_equal Date.parse("01.08.2011"), @flat[:available_on]
+    assert_equal Date.parse("30.09.2011"), @flat[:available_until]
+  end
+  
+  it "parses price" do
+    assert_equal 320, @flat[:price]      
+  end
+  
+  it "parses neighbourhood" do
+    assert_equal "Schwabing-West", @flat[:neighbourhood]    
+  end
+  
+  it "parses street" do
+    assert_equal "Occamstr 21", @flat[:street]
+  end    
   
   private
   
     def hpricot_doc_for(file)
       open(File.join(File.dirname(__FILE__), "/test_data/#{file}.html")) { |f| Hpricot(f) }
     end
+  
+end
+
+describe "it parses additional info on solo flats" do
   
 end
