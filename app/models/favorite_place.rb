eaa18dfa0fde4c3b41eb5dffdcc87af094f18e5b
Phillip Oertel <me@phillipoertel.com>
2011-10-10 01:47:47 +0200

    The suck.

--- app/models/favorite_place.rb
+++ app/models/favorite_place.rb
@@ -3,7 +3,7 @@ class FavoritePlace < ActiveRecord::Base
   validates_presence_of :street, :city
   
   # FIXME it would be better to stub the request and have it run, than to prevent it completely
-  acts_as_gmappable(:process_geocoding => !Rails.env.test?)
+  acts_as_gmappable(:process_geocoding => false)
 
   def full_address
     [street, city, country].join(", ")
