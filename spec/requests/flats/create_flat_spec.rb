0d979678ee221e84b069c9bd0c7848f3f57f57f2
Phillip Oertel <me@phillipoertel.com>
2011-10-10 00:23:49 +0200

    Made test less brittle after reading about Incidental Detail and Duplication in acceptance tests.

--- spec/requests/flats/create_flat_spec.rb
+++ spec/requests/flats/create_flat_spec.rb
@@ -1,31 +1,33 @@
 require 'spec_helper'
+require 'ostruct'
 
 feature "Creating a flat" do
 
-  scenario "Creating a flat from scratch", :type => :smoke do
+  def go_to_create_form
     visit(root_path)
     click_on('Add flat')
     click_on('Continue')
-    f = Factory.build(:flat)
-    fill_in('Street', :with => f.street)
-    fill_in('Neighbourhood', :with => f.neighbourhood)
-    fill_in('Square meters', :with => f.square_meters)
-    fill_in('Price', :with => f.price)
-    click_on('Create')
-    page.should have_content('flat was successfully created.')
-    page.should have_css('h2', :text => f.street)
-    page.should have_content(f.neighbourhood)
   end
   
-  scenario "i get a validation error when price info is missing" do
-    visit(root_path)
-    click_on('Add flat')
-    click_on('Continue')
-    fill_in('Street', :with => 'Kazmairstraße 42')
-    fill_in('Neighbourhood', :with => 'Westend')
-    fill_in('Square meters', :with => '25')
+  def fill_form(flat)
+    fill_in('Street', :with => flat.street)
+    fill_in('Neighbourhood', :with => flat.neighbourhood)
+    fill_in('Square meters', :with => flat.square_meters)
+    fill_in('Price', :with => flat.price)
     click_on('Create')
-    page.should have_content('2 errors prohibited this flat from being saved')
+  end
+  
+  scenario "Creating a flat from scratch", :type => :smoke do
+    go_to_create_form
+    fill_form(Factory.build(:flat))
+    page.should have_content('flat was successfully created.')
+  end
+  
+  scenario "I get a validation error when required info is missing" do
+    go_to_create_form
+    fill_form(Factory.build(:flat, :street => nil, :price => nil))
+    show!
+    page.should have_css('h2', :text => /error/)
   end
   
 end
