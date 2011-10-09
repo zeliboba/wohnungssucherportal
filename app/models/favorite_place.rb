04378bbbe2b044611c869aa54a39bcb793e84ab1
Phillip Oertel <me@phillipoertel.com>
2011-10-10 01:49:39 +0200

    The doublesuck

--- app/models/favorite_place.rb
+++ app/models/favorite_place.rb
@@ -3,7 +3,7 @@ class FavoritePlace < ActiveRecord::Base
   validates_presence_of :street, :city
   
   # FIXME it would be better to stub the request and have it run, than to prevent it completely
-  acts_as_gmappable(:process_geocoding => false)
+  acts_as_gmappable(:process_geocoding => !Rails.env.test?)
 
   def full_address
     [street, city, country].join(", ")
