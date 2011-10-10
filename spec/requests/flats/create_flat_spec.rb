7c9f7fc6a6b94aaa6dbdc33787f60397fba25a16
Phillip Oertel <me@phillipoertel.com>
2011-10-10 02:04:43 +0200

    Gettin' all BDD-y

--- spec/requests/flats/create_flat_spec.rb
+++ spec/requests/flats/create_flat_spec.rb
@@ -3,13 +3,10 @@ require 'ostruct'
 
 feature "Creating a flat" do
 
-  def go_to_create_form
+  def create_flat_with(flat)
     visit(root_path)
     click_on('Add flat')
     click_on('Continue')
-  end
-  
-  def fill_form(flat)
     fill_in('Street', :with => flat.street)
     fill_in('Neighbourhood', :with => flat.neighbourhood)
     fill_in('Square meters', :with => flat.square_meters)
@@ -18,14 +15,12 @@ feature "Creating a flat" do
   end
   
   scenario "Create a flat with all required attributes", :type => :smoke do
-    go_to_create_form
-    fill_form(Factory.build(:flat))
+    create_flat_with(Factory.build(:flat))
     page.should have_content('flat was successfully created.')
   end
   
   scenario "I get an error when required attributes are missing" do
-    go_to_create_form
-    fill_form(Factory.build(:flat, :street => nil, :price => nil))
+    create_flat_with(Factory.build(:flat, :street => nil))
     page.should have_css('h2', :text => /error/)
   end
   
